950ac0a1a94777cf2780d1f28c261beef868c305
shimital <shimital269@gmail.com>
2020-12-07 11:58:40 +0200

    add resize event listeners

--- src/app/ellipsis.directive.ts
+++ src/app/ellipsis.directive.ts
@@ -1,33 +1,59 @@
-import { AfterViewInit, Directive, ElementRef } from '@angular/core';
+import { AfterViewInit, Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
     selector: '[appEllipsis]'
 })
 export class EllipsisDirective implements AfterViewInit {
 
+    private ellipsisEl: HTMLElement;
+    private textContainer: HTMLElement;
+
     constructor (private readonly el: ElementRef) {
     }
 
     ngAfterViewInit (): void {
-        const textContainer = <HTMLElement> this.el.nativeElement;
-        const textContainerHeight = textContainer.clientHeight;
-        const textContainerParent = textContainer.parentElement;
+        this.textContainer = <HTMLElement> this.el.nativeElement;
+        this.checkAndAddEllipsis();
+    }
+
+    @HostListener('window:resize') checkAndAddEllipsis (): void {
+        this.resetTextContainer();
+        const textContainerHeight = this.textContainer.clientHeight;
+        const textContainerParent = this.textContainer.parentElement;
         const textContainerParentHeight = textContainerParent.clientHeight;
 
         if (textContainerHeight > textContainerParentHeight) {
-            textContainer.style.height = '100%';
-            textContainer.style.overflow = 'hidden';
+            this.normalizeTextContainer();
+
+            if (this.ellipsisEl) {
+                return;
+            }
+
             const ellipsisEl = document.createElement('span');
 
             Object.assign(ellipsisEl.style, {
                 position: 'absolute',
                 bottom: '10px',
-                right: '4px'
+                right: '6px'
             });
 
             ellipsisEl.textContent = '...';
-            textContainerParent.appendChild(ellipsisEl);
+            this.ellipsisEl = ellipsisEl;
+            textContainerParent.appendChild(this.ellipsisEl);
+        } else if (this.ellipsisEl) {
+            this.ellipsisEl.remove();
+            this.ellipsisEl = null;
         }
+    }
+
+    private resetTextContainer (): void {
+        this.textContainer.style.height = 'auto';
+        this.textContainer.style.overflow = 'auto';
+    }
+
+    private normalizeTextContainer (): void {
+        this.textContainer.style.height = '100%';
+        this.textContainer.style.overflow = 'hidden';
     }
 
 }
