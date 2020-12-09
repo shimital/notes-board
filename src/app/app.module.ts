1497d46a5811c4b7ff87ef95f0f69e95257ec955
shimital <shimital269@gmail.com>
2020-12-09 09:33:12 +0200

    remove login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NotesDisplayComponent } from './notes-display/notes-display.component';
 import { NoteComponent } from './note/note.component';
@@ -9,7 +8,6 @@ import { NewNoteFormComponent } from './new-note-form/new-note-form.component';
 import { FormsModule } from '@angular/forms';
 import { UpdateNoteFormComponent } from './update-note-form/update-note-form.component';
 import { DeleteNoteConfirmComponent } from './delete-note-confirm/delete-note-confirm.component';
-import { LoginComponent } from './login/login.component';
 import { DialogModule } from './dialog/dialog.module';
 import { EllipsisDirective } from './ellipsis.directive';
 
@@ -21,12 +19,10 @@ import { EllipsisDirective } from './ellipsis.directive';
         NewNoteFormComponent,
         UpdateNoteFormComponent,
         DeleteNoteConfirmComponent,
-        LoginComponent,
         EllipsisDirective
     ],
     imports: [
         BrowserModule,
-        AppRoutingModule,
         BrowserAnimationsModule,
         FormsModule,
         DialogModule
